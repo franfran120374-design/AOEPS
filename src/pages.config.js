514eadbda5c707a4e36bf792427a83596c49492a
sandra mercier <franfran120374@gmail.com>
2026-07-24 12:01:54 +0200

    Add pages configuration for Evaluation page

---
+++ src/pages.config.js
@@ -0,0 +1,11 @@
+import Evaluation from './pages/Evaluation';
+
+
+export const PAGES = {
+    "Evaluation": Evaluation,
+}
+
+export const pagesConfig = {
+    mainPage: "Evaluation",
+    Pages: PAGES,
+};
